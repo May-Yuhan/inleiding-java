6b155c96d946f3f391b49288a953b1dee7e7ee21
may-yuhan <may-yuhan@qq.com>
2018-11-01 14:28:55 +0100

    opdrachten tm 4.2

--- src/H11/Opdracht_11_3.java
+++ src/H11/Opdracht_11_3.java
@@ -5,17 +5,28 @@ import java.awt.*;
 
 public class Opdracht_11_3 extends Applet {
 
+    String string;
+
     public void init(){
 
     }
 
     public void paint (Graphics g){
 
-      int a;
+      int a = 0;
       int b = 1;
+      int c = 0;
 
 
-        for(a = 0; a < 10; ++a);
+        for(a = 0; a < 10; ++ a);
+
+        b += a;
+
+        c = a + b;
+
+        string.valueOf(c);
+        System.out.println(c);
+
 
 
     }
